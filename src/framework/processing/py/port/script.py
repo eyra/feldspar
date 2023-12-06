722005b91a4da0f0495db0cb6380a883bea2cd8e
Jeroen <jeroen@adaptiveconstant.com>
2023-12-06 08:45:56 +0100

    Added date filter

--- src/framework/processing/py/port/script.py
+++ src/framework/processing/py/port/script.py
@@ -4,6 +4,97 @@ from port.api.commands import (CommandSystemDonate, CommandSystemExit, CommandUI
 import pandas as pd
 import zipfile
 
+ExtractionResult = namedtuple("ExtractionResult", ["id", "title", "data_frame"])
+filter_start_date = datetime(2017, 1, 1)
+
+
+def get_in(dct, *key_path):
+    for key in key_path:
+        dct = dct.get(key)
+        if dct is None:
+            return
+    return dct
+
+
+def parse_json_to_dataframe(parsed_dict):
+    data = []
+    for obj in parsed_dict["timelineObjects"]:
+        if "activitySegment" not in obj:
+            continue
+
+        segment = obj["activitySegment"]
+        activity_type = segment["activityType"]
+
+        if activity_type not in {"WALKING", "CYCLING","RUNNING"}:
+            continue
+
+        start_timestamp_str = segment["duration"]["startTimestamp"]
+        start_timestamp = datetime.fromisoformat(
+            start_timestamp_str[:-1]
+        )  # remove the 'Z'
+
+        if start_timestamp < filter_start_date:
+            continue
+
+        if meters := get_in(segment, "waypointPath", "distanceMeters"):
+            distance_meters = meters
+        elif meters := get_in(segment, "simplifiedRawPath", "distanceMeters"):
+            distance_meters = meters
+        elif meters := segment.get("distance"):
+            distance_meters = meters
+        else:
+            continue
+
+        data.append([start_timestamp, activity_type, distance_meters])
+
+    return pd.DataFrame(
+        data, columns=["startTimestamp", "activityType", "distanceMeters"]
+    )
+
+
+def aggregate_distance_by_day_activity(df):
+    # Format the startTimestamp to "year-month-day" format
+    df["startTimestamp"] = df["startTimestamp"].dt.strftime("%Y-%m-%d")
+
+    # Group by formatted date and activityType, then aggregate the distance
+    aggregated_df = (
+        df.groupby(["startTimestamp", "activityType"])["distanceMeters"]
+        .sum()
+        .reset_index()
+    )
+
+    return aggregated_df
+
+
+def extract(df):
+    aggregated_df = aggregate_distance_by_day_activity(df)
+    aggregated_df["Afstand in km"] = aggregated_df["distanceMeters"] / 1000
+
+    results = []
+    for activity_type, title in [
+        ("WALKING", {"en": "Walking", "nl": "Gewandeld"}),
+        ("CYCLING", {"en": "Cycling", "nl": "Gefietst"}),
+        ("RUNNING", {"en": "Running", "nl": "Hardgelopen"}),
+    ]:
+        df = aggregated_df.loc[aggregated_df["activityType"] == activity_type]
+        if len(df) == 0:
+            continue
+
+        df["Datum"] = df["startTimestamp"]
+        df = (
+            df.drop(columns=["distanceMeters", "activityType", "startTimestamp"])
+            .reset_index(drop=True)
+            .reindex(columns=["Datum", "Afstand in km"])
+        )
+        results.append(
+            ExtractionResult(
+                id=activity_type.lower(),
+                title=props.Translatable(title),
+                data_frame=df,
+            )
+        )
+    return results
+
 
 def process(sessionId):
     yield donate(f"{sessionId}-tracking", '[{ "message": "user entered script" }]')
