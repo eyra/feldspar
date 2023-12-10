72f36f76a5ceceea311f4af9e2004b194790f4f6
Jeroen <jeroen@adaptiveconstant.com>
2023-12-10 17:45:43 +0100

    Removed end page for integration

--- src/framework/processing/py/port/script.py
+++ src/framework/processing/py/port/script.py
@@ -169,9 +169,6 @@ def process(sessionId):
                 meta_data.append(("debug", f"{platform}: donate consent data"))
                 yield donate(f"{sessionId}-{platform}", consent_result.value)
 
-    yield exit(0, "Success")
-    yield render_end_page()
-
 
 def render_end_page():
     page = props.PropsUIPageEnd()
