7d2c7c77d0a2708163eb05e0dd336da56ae44279
emielvdveen <emiel@applike.nl>
2023-12-13 00:12:17 +0100

    Fixed use-case: continue with empty/wrong package

--- src/framework/processing/py/port/script.py
+++ src/framework/processing/py/port/script.py
@@ -97,37 +97,24 @@ def extract(df):
 
 
 def process(sessionId):
-    yield donate(f"{sessionId}-tracking", '[{ "message": "user entered script" }]')
-
-    platforms = ["Twitter", "Facebook", "Instagram", "Youtube"]
-
-    subflows = len(platforms)
-    steps = 2
-    step_percentage = (100/subflows)/steps
-
-    # progress in %
-    progress = 0
-
-    for index, platform in enumerate(platforms):
-        meta_data = []
-        meta_data.append(("debug", f"{platform}: start"))
+    meta_data = []
+    meta_data.append(("debug", f"start"))
 
     # STEP 1: select the file
     data = None
     while True:
-        print("A")
         promptFile = prompt_file()
-        print("B")
-        fileResult = yield render_donation_page(promptFile, 33)
-        print("C")
+        fileResult = yield render_donation_page(promptFile)
         if fileResult.__type__ == "PayloadString":
             meta_data.append(("debug", f"extracting file"))
+            print("A")
             extractionResult = extract_data_from_zip(fileResult.value)
+            print("B")
             if extractionResult == "invalid":
                 meta_data.append(
                     ("debug", f"prompt confirmation to retry file selection")
                 )
-                retry_result = yield render_donation_page(retry_confirmation(), 33)
+                retry_result = yield render_donation_page(retry_confirmation())
                 if retry_result.__type__ == "PayloadTrue":
                     meta_data.append(("debug", f"skip due to invalid file"))
                     continue
@@ -136,7 +123,7 @@ def process(sessionId):
                     break
             if extractionResult == "no-data":
                 retry_result = yield render_donation_page(
-                    retry_no_data_confirmation(), 33
+                    retry_no_data_confirmation()
                 )
                 if retry_result.__type__ == "PayloadTrue":
                     continue
@@ -159,29 +146,21 @@ def process(sessionId):
             meta_data.append(("debug", f"skip to next step"))
             break
 
-        # STEP 2: ask for consent
-        progress += step_percentage
-        if data is not None:
-            meta_data.append(("debug", f"{platform}: prompt consent"))
-            prompt = prompt_consent(platform, data, meta_data)
-            consent_result = yield render_donation_page(platform, prompt, progress)
-            if consent_result.__type__ == "PayloadJSON":
-                meta_data.append(("debug", f"{platform}: donate consent data"))
-                yield donate(f"{sessionId}-{platform}", consent_result.value)
+    # STEP 2: ask for consent
+    meta_data.append(("debug", f"prompt consent"))
+    prompt = prompt_consent(data, meta_data)
+    consent_result = yield render_donation_page(prompt)
+    if consent_result.__type__ == "PayloadJSON":
+        meta_data.append(("debug", f"donate consent data"))
+        yield donate(f"{sessionId}", consent_result.value)
 
 
-def render_end_page():
-    page = props.PropsUIPageEnd()
-    return CommandUIRender(page)
-
-
-def render_donation_page(body, progress):
+def render_donation_page(body):
     header = props.PropsUIHeader(
         props.Translatable({"en": "Google location", "nl": "Google locatie"})
     )
 
-    footer = props.PropsUIFooter(progress)
-    page = props.PropsUIPageDonation("google-location", header, body, footer)
+    page = props.PropsUIPageDonation("google-location", header, body)
     return CommandUIRender(page)
 
 
@@ -220,12 +199,16 @@ def prompt_file():
     return props.PropsUIPromptFileInput(description, extensions)
 
 
-def prompt_consent(tables, meta_data):
+def prompt_consent(data, meta_data):
     log_title = props.Translatable({"en": "Log messages", "nl": "Log berichten"})
-    tables = [
-        props.PropsUIPromptConsentFormTable(table.id, table.title, table.data_frame)
-        for table in tables
-    ]
+
+    tables = []
+    if data is not None:
+        tables = [
+            props.PropsUIPromptConsentFormTable(table.id, table.title, table.data_frame)
+            for table in data
+        ]
+
     meta_frame = pd.DataFrame(meta_data, columns=["type", "message"])
     meta_table = props.PropsUIPromptConsentFormTable("log_messages", log_title, meta_frame)
     return props.PropsUIPromptConsentForm([table], [meta_table])
