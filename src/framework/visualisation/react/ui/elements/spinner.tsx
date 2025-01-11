85cdaef08baae9e22b8ee1192d5c65aa85fd615a
Jeroen <jeroen@adaptiveconstant.com>
2025-01-11 13:27:54 +0100

    Upgrade dependencies and replace lottie-react with @lottiefiles/dotlottie-react in Spinner component

--- src/framework/visualisation/react/ui/elements/spinner.tsx
+++ src/framework/visualisation/react/ui/elements/spinner.tsx
@@ -1,26 +1,28 @@
-import { Weak } from '../../../../helpers'
-import { PropsUISpinner } from '../../../../types/elements'
+import { Weak } from "../../../../helpers";
+import { PropsUISpinner } from "../../../../types/elements";
 
-import React from 'react'
-import Lottie from 'lottie-react'
-import spinnerLight from '../../../../../assets/lottie/spinner-light.json'
-import spinnerDark from '../../../../../assets/lottie/spinner-dark.json'
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import spinnerLight from "../../../../../assets/lottie/spinner-light.json";
+import spinnerDark from "../../../../../assets/lottie/spinner-dark.json";
 
-type Props = Weak<PropsUISpinner>
+type Props = Weak<PropsUISpinner>;
 
-export const Spinner = ({ spinning = true, color = 'light' }: Props): JSX.Element => {
-  function animationData (): unknown {
-    if (color === 'dark') {
-      return spinnerDark
+export const Spinner = ({
+  spinning = true,
+  color = "light",
+}: Props): JSX.Element => {
+  function animationData(): string {
+    if (color === "dark") {
+      return JSON.stringify(spinnerDark);
     }
-    return spinnerLight
+    return JSON.stringify(spinnerLight);
   }
 
   return (
-    <div id='spinner' className='flex flex-row items-center gap-4'>
-      <div className='w-5 h-5'>
-        <Lottie animationData={animationData()} loop={spinning} />
+    <div id="spinner" className="flex flex-row items-center gap-4">
+      <div className="w-5 h-5">
+        <DotLottieReact data={animationData()} loop={spinning} autoplay />
       </div>
     </div>
-  )
-}
+  );
+};
