6bbd9988411b314fb7439d7a240127014d86c1d3
Jeroen <jeroen@adaptiveconstant.com>
2025-01-18 14:54:52 +0100

    Made Feldspar a React component

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,16 @@
 import './fonts.css'
 import './framework/styles.css'
-import Assembly from './framework/assembly'
-import { Bridge } from './framework/types/modules'
-import LiveBridge from './live_bridge'
-import FakeBridge from './fake_bridge'
+import { createRoot } from 'react-dom/client'
+import { FeldsparComponent } from './components/FeldsparComponent'
 
 const rootElement = document.getElementById('root') as HTMLElement
+const root = createRoot(rootElement)
 
-const workerFile = new URL('./framework/processing/py_worker.js', import.meta.url)
-const worker = new Worker(workerFile)
+const workerUrl = new URL('./framework/processing/py_worker.js', import.meta.url).href
 
-let assembly: Assembly
-
-const run = (bridge: Bridge, locale: string): void => {
-  assembly = new Assembly(worker, bridge)
-  assembly.visualisationEngine.start(rootElement, locale)
-  assembly.processingEngine.start()
-}
-
-if (process.env.REACT_APP_BUILD !== 'standalone' && process.env.NODE_ENV === 'production') {
-  // Setup embedded mode (requires to be embedded in iFrame)
-  console.log('Initializing bridge system')
-  LiveBridge.create(window, run)
-} else {
-  // Setup local development mode
-  console.log('Running with fake bridge')
-  run(new FakeBridge(), 'en')
-}
-
-const observer = new ResizeObserver(() => {
-  const height = window.document.body.scrollHeight
-  const action = 'resize'
-  window.parent.postMessage({ action, height }, '*')
-})
-
-observer.observe(window.document.body)
+root.render(
+  <FeldsparComponent 
+    workerUrl={workerUrl}
+    standalone={process.env.REACT_APP_BUILD === 'standalone'}
+  />
+)
